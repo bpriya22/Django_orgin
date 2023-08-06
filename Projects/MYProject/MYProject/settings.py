8e2a2173675adc92c9e9f8bd0941c4b9c82de619
rrm <priyanka.balakrishna2@gmail.com>
2023-08-07 00:05:41 +0530

    Passing Data to HTML

--- Projects/MYProject/MYProject/settings.py
+++ Projects/MYProject/MYProject/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent      #this is how templates folder is found
 
 
 # Quick-start development settings - unsuitable for production
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'MYProject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR,'templates')],
+        'DIRS': [os.path.join(BASE_DIR,'templates')],   #telling django where it finds templates
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -117,6 +117,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]    #TELLING DJANGO WHERE U'LL FIND ALL THE STATIC FILES...--->these 2 things are must
+STATIC_ROOT = os.path.join(BASE_DIR,'assets') #--->these 2 things are must- for static files.
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
---
+++ Projects/MYProject/TravelloApp/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+from .models import Destination
+# Create your views here.
+
+def index(request):
+    dest1=Destination()
+    dest1.name='Mumbai'
+    dest1.desc="The City that never Sleeps"
+    dest1.price= 700
+
+    dest2=Destination()
+    dest2.name="Mangalore"
+    dest2.desc = "A city of Beaches"
+    dest2.price=654
+
+    dest3=Destination()
+    dest3.name="Bangalore"
+    dest3.desc="The Silicon City of India"
+    dest3.price=890
+
+    dests=[dest1,dest2, dest3]
+
+    return render(request,"index.html",{'dests':dests})  #object created should be here.
