f216f44eb3fcb48f624b726eb657e39af719f785
rrm <priyanka.balakrishna2@gmail.com>
2023-08-15 19:58:44 +0530

    Admin Panel

--- Projects/MYProject/MYProject/settings.py
+++ Projects/MYProject/MYProject/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'TravelloApp.apps.TravelloappConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -76,8 +77,12 @@ WSGI_APPLICATION = 'MYProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'telusko',
+        'USER': 'postgres',
+        'PASSWORD' : 'tiger',
+        'HOST' : 'localhost',
+
     }
 }
 
