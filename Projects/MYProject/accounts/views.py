3d8b8cb54b758a82e42e7bd07ef6ae5d545166e0
rrm <priyanka.balakrishna2@gmail.com>
2023-08-27 22:39:37 +0530

    Passing messages in django

--- Projects/MYProject/accounts/views.py
+++ Projects/MYProject/accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.models import User, auth
+from django.contrib import messages
 
 # Create your views here.
 def register(request):
@@ -13,16 +14,22 @@ def register(request):
         email = request.POST['email']
         if password1 == password2:
             if User.objects.filter(username=username).exists():
-                print("username taken")
+                # print("username taken")
+                messages.info(request,'Username Taken')
+                return redirect('register')
             elif User.objects.filter(email=email).exists():
-                print("email already in use")
+                # print("email already in use")
+                messages.info(request,'Email Taken')
+                return redirect('register')
             else:
                 #when we want the form data to be stored on database. 4 lines necessary
                 user=User.objects.create_user(username=username,password=password1,email=email,first_name=first_name,last_name=last_name)
                 user.save()
-                print('user created')
+                #print('user created')
+                messages.info(request,"User Created")
         else:
-            print('password not matching')
+            messages.info(request,'password not matching')
+            return redirect('register')
         return redirect('/') #to redirect to home page
 
     else:
